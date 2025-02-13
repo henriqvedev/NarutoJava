30f477e8c8ae7a7c77326963d609f90798ae42d1
henriqvedev <dtrr2k21@gmail.com>
2025-02-13 19:49:35 -0300

    Getters e Setters pra encapsular

--- src/edu/Main.java
+++ src/edu/Main.java
@@ -5,14 +5,16 @@ public class Main {
 
         //obj 1
         Uzumaki naruto = new Uzumaki();
-        naruto.nome = "Naruto Uzumaki";
+        naruto.setNome("Naruto Uzumaki");
+        naruto.getNome();
+        System.out.println("Meu nome é:" + naruto.getNome());
+        naruto.idade = 16;
         naruto.temBijuu = true;
         naruto.ataqueKunai();
 
 
         //obj 2
         Uchiha sasuke = new Uchiha();
-        sasuke.nome = "Sasuke Uchiha";
         sasuke.ataqueKunai();
         sasuke.ataqueKunai(3);
 
--- src/edu/Ninja.java
+++ src/edu/Ninja.java
@@ -1,9 +1,20 @@
 package edu;
 
 public class Ninja {
-    String nome;
+    private String nome;
     String aldeia;
     int idade;
+    int nivelDeMissao;
+
+    /* Getter - Criar getter para mostrar para o usuário */
+    public String getNome (){
+        return nome;
+    }
+
+    /* Setter - Settar o valor da variavel*/
+    public void setNome(String nome){
+        this.nome = nome;
+    }
 
     public void ataqueKunai(){
         System.out.println("Kunai lançada!");
